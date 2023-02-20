4b5be6c17357665987c77fb80b322348129d18b8
201 <86655425+seoill@users.noreply.github.com>
2023-02-21 03:42:26 +0900

    230221
    js 분리

--- script/script.js
+++ script/script.js
@@ -205,77 +205,7 @@
         }
     }
     const newAbout = new about();
-    class service{
-        init(){
-            this.service();
-            console.log('service start');
-        }
-        service(){
-            const win = $(window);
-            let isAnimated = false;
-            win.on('wheel scroll touchmove',function(e){
-                if(!isAnimated){
-                    if(e.originalEvent.deltaY<0) return;
-                    $('#sec1').addClass('on');
-                    $('.title-box h2').eq(0).addClass('ani-fadeIn');
-                    $('.title-box h2').eq(1).addClass('ani-fadeIn');
-                    $('.text-box').addClass('ani-up2');                    
-                    isAnimated = true;
-                    setTimeout(function(){
-                        $('html').css({'overflow':'scroll'});
-                    });
-                }
-                if(e.originalEvent.deltaY<0 && win.scrollTop() < 10){
-                    if(isAnimated == true){
-                        $('html').css({'overflow':'hidden'});
-                        $('#sec1').removeClass('on');
-                        $('.title-box h2').eq(0).removeClass('ani-fadeIn');
-                        $('.title-box h2').eq(1).removeClass('ani-fadeIn');
-                        $('.text-box').removeClass('ani-up2');
-                        isAnimated=false;
-                    }
-                    
-                }
-            });
-            $('.area2-slider').bxSlider({
-                pager:false,
-                controls:false,
-                slideMargin: 30,
-                minSlides:3,
-                maxSlides:3,
-                moveSlides:1,
-                slideWidth: 370,
-                infiniteLoop:false
-            });
-            $('.area3-slider').bxSlider({
-                pager:false,
-                controls:false,
-                slideMargin: 30,
-                minSlides:3,
-                maxSlides:3,
-                moveSlides:1,
-                slideWidth: 370,
-                infiniteLoop:false
-            });
-        }
-    }
-    const newService = new service();
-
-    $(document).ready(function(){
-        let pageLocation = String(document.location).split('/');
-        let selectedPage = pageLocation[4].replace('.html','');
-    
-
-        function linkCheck(){
-            if(selectedPage == 'index.html' || 'index.html#') newAbout.init();
-            if(selectedPage == 'service.html' || 'service.html#') newService.init();
-            if(selectedPage == 'story.html' || 'story.html#'){
-            }
-            if(selectedPage == 'people.html' || 'people.html#'){
-            }
-        }
-        linkCheck();
-    });
+    newAbout.init();
 
 })(jQuery);
 
